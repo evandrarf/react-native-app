c4248058c9f46ebce41a9e773113aef66da8530d
Evandra Raditya <evandraraditya049@gmail.com>
2022-04-03 10:05:33 +0700

    Initial commit

---
+++ components/index.js
@@ -0,0 +1,18 @@
+import HomeHeader from "./HomeHeader";
+import FocusedStatusBar from "./FocusedStatusBar";
+import NFTCard from "./NFTCard";
+import { CircleButton, RectButton } from "./Button";
+import { SubInfo } from "./SubInfo";
+import DetailDesc from "./DetailDesc";
+import DetailsBid from "./DetailsBid";
+
+export {
+  HomeHeader,
+  FocusedStatusBar,
+  NFTCard,
+  CircleButton,
+  RectButton,
+  DetailDesc,
+  DetailsBid,
+  SubInfo,
+};
